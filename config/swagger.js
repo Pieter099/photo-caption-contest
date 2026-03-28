b4e4b807fba1e0fce1793d1634b233ba8b781800
Pieter Lourens <1pieterlourens@gmail.com>
2026-03-28 12:51:50 +0200

    Added swagger file

---
+++ config/swagger.js
@@ -0,0 +1,23 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Photo Caption Contest API',
+      version: '1.0.0',
+      description: 'API for managing photo caption contests'
+    },
+     servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server'
+      }
+    ]
+  },
+  apis: ['./routes/*.js'] // Path to the API routes
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
