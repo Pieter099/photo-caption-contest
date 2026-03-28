798f0eac7ad0aa39980328155730bcfbf302fcf5
Pieter Lourens <1pieterlourens@gmail.com>
2026-03-28 14:36:11 +0200

    Added swagger documentation

--- routes/login.js
+++ routes/login.js
@@ -5,6 +5,44 @@ const bcrypt = require('bcrypt');
 const { User } = require('../models');
 const jwt = require('jsonwebtoken');
 
+// ==============================
+// POST LOGIN
+// ==============================
+/** @swagger
+ * /login:
+ *   post:
+ *     summary: User login
+ *     description: Authenticate a user and return a JWT token.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "user@example.com"
+ *              password:
+ *                 type: string
+ *                 example: "password123"
+    *     responses:
+ *       200:
+ *         description: Successful login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: "your_jwt_token"
+ *       404:
+ *         description: User not found
+ *       401:
+ *         description: Invalid password
+ */
+
 router.post('/', async (req, res) => {
 
     const { email, password } = req.body;
--- routes/register.js
+++ routes/register.js
@@ -4,6 +4,48 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
+// ==============================
+// POST REGISTER
+// ==============================
+/** @swagger
+ * /register:
+ *   post:
+ *     summary: User registration
+ *     description: Register a new user with username, email, and password.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: "john_doe"
+ *               email:
+ *                 type: string
+ *                 example: "john@example.com"
+ *               password:
+ *                 type: string
+ *                 example: "password123"
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "User registered successfully"
+ *                 userId:
+ *                   type: string
+ *                   example: "user_id"
+ *       500:
+ *         description: Internal server error
+ */
+
 router.post('/', async (req, res) => {
     try {
         const { username,email,  password } = req.body;
