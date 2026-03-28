6de766d2f3d395d7af65da01a059912e3f0a2b97
Pieter Lourens <1pieterlourens@gmail.com>
2026-03-28 12:59:11 +0200

    Added swagger to main file

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const registerRoutes = require('./routes/register');
 const loginRoutes = require('./routes/login');
 const imagesRoute = require('./routes/images');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
+
 app.use(cors());
 app.use(express.json());
 
@@ -17,3 +20,8 @@ app.use('/images', imagesRoute);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// ==============================
+// Swagger API Documentation
+// ==============================
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
