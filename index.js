e567063d169a680599e6e8e2581078bca791c666
Pieter Lourens <1pieterlourens@gmail.com>
2026-05-15 17:19:16 +0200

    Switched Sequelize to PostgreSQL

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const registerRoutes = require('./routes/register');
 const loginRoutes = require('./routes/login');
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
 
